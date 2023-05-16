78e6d6844c0cf7833c59553db510645781c9a0cb
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-05-16 09:42:02 +0200

    Finished PriorityQueueArray

--- PriorityQueueArray.py
+++ PriorityQueueArray.py
@@ -1,2 +1,31 @@
+from typing import Optional
+
 class PriorityQueueArray:
-    pass
+    def __init__(self) -> None:
+        self.queue: list[int] = []
+
+    def __str__(self) -> str:
+        return ' '.join([str(item) for item in self.queue])
+
+    def is_empty(self) -> bool:
+        """ Return True if the queue is empty """
+
+        return len(self.queue) == 0
+
+    def insert(self, item: int) -> None:
+        """ Add new item to the queue """
+
+        self.queue.append(item)
+
+    def pop(self) -> Optional[int]:
+        """ Remove item from the queue """
+
+        if self.is_empty():
+            return None
+
+        index = 0
+        for i in range(1, len(self.queue)):
+            if self.queue[i] < self.queue[index]:
+                index = i
+
+        return self.queue.pop(index)
